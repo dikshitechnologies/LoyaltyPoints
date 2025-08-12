ce8a90bcad0f802febb9b44750967203ca65a164
dikshitechnologies <dikshitechnologies.developer@gmail.com>
2025-08-12 10:56:53 +0530

    ratefixed

--- src/RateFixingScreen.js
+++ src/RateFixingScreen.js
@@ -104,7 +104,7 @@ useFocusEffect(
             console.log("Sending to Point→Amount API:", payload);
 
             const res = await axios.post(
-                "http://dikshi.ddns.net/loyaltypoints/api/Ratefixing/RedeemPointAmount",
+                "https://dikshi.ddns.net/loyaltypoints/api/Ratefixing/RedeemPointAmount",
                 payload
             );
             console.log("Tab 2 Response:", res.data);
@@ -124,8 +124,8 @@ const AddPoints = async ()=>{
     console.log(response)
     if(response.status == 200){
       
-      setAmount1(response.data[0].amount);
-      setPoints1(response.data[0].point);
+      setAmount1(response.data[0].amount.toString());
+      setPoints1(response.data[0].point.toString());
     }
      else {
         handleStatusCodeError(response.status, "Error deleting data");
@@ -149,10 +149,11 @@ catch (error) {
 const RedeemAmount = async ()=>{
   try{
     const response = await axios.get(`${BASE_URL}Ratefixing/Redeempoints/${fcomCode}`)
+       console.log(response.data)
     if(response.status == 200){
-      console.log(response.data)
-      setAmount2(response.data[0].fpointVal);
-      setPoints2(response.data[0].point);
+      console.log(response.data[0]) 
+      setAmount2(response.data[0].fpointVal.toString()); 
+      setPoints2(response.data[0].point.toString());
     }
      else {
         handleStatusCodeError(response.status, "Error deleting data");
